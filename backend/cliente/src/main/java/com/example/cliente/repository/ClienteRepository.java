30e6dc8f148a8f81a3b59202206e9d186c3177fb
Leticia <leticiamedeirosnasc@gmail.com>
2023-04-17 21:00:46 -0300

    Alteração no formulário de captação de clientes

--- backend/cliente/src/main/java/com/example/cliente/repository/ClienteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.cliente.repository;
-
-import com.example.cliente.model.Cliente;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClienteRepository extends MongoRepository<Cliente,String> {
-}
